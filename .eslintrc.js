0f08384b9484ca60bf8daded37e0db3ae6297166
MBuchalik <dev@martin-buchalik.de>
2023-02-04 10:47:30 +0100

    Add custom linter rules

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+  },
+
+  // Ignore all folders except for '/src'.
+  ignorePatterns: ['/*', '!/src'],
+
+  overrides: [
+    {
+      files: ['*.ts'],
+      extends: ['eslint-config-mbuchalik'],
+      rules: {
+        'import/no-unresolved': ['error', { ignore: ['vscode'] }],
+      },
+    },
+  ],
+};
